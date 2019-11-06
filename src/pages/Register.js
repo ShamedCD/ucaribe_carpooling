fd2d7b0dfa2bf072172e6a5f12dfa423c4ba541f
Emmanuel Yupit <emmanuelyupit08@gmail.com>
2019-11-06 17:34:54 -0500

    WIP regitser

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,4 +1,11 @@
-import { IonPage } from '@ionic/react';
+import {
+  IonPage,
+  IonInput,
+  IonItem,
+  IonLabel,
+  IonContent,
+  IonText,
+} from '@ionic/react';
 import React, { Component } from 'react';
 import Header from '../components/Header/index';
 
@@ -7,6 +14,24 @@ export class Register extends Component {
     return (
       <IonPage>
         <Header title="Registro"></Header>
+        <IonContent>
+          <IonItem>
+            <IonLabel position="stacked">
+              Nombre <IonText color="danger">*</IonText>
+            </IonLabel>
+            <IonInput></IonInput>
+          </IonItem>
+          <IonItem>
+            <IonLabel position="stacked">
+              Apellidos <IonText color="danger">*</IonText>
+            </IonLabel>
+            <IonInput></IonInput>
+          </IonItem>
+          <IonItem>
+            <IonLabel position="stacked">Carrera</IonLabel>
+            <IonInput></IonInput>
+          </IonItem>
+        </IonContent>
       </IonPage>
     );
   }
